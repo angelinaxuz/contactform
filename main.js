b273ad3879b392a217178ebe7105ac58cb32994b
Angelina Khuziakhmetova <xuz.angelina@gmail.com>
2021-06-11 15:31:06 +0500

    add validation

--- main.js
+++ main.js
@@ -1,22 +1,54 @@
-let myForm = document.forms.myform
+const myForm = document.querySelector('#myForm')
 
 myForm.addEventListener('submit', (e) => {
     e.preventDefault()
     let formData = new FormData(myForm)
     let values = Object.fromEntries(formData.entries())
+    let {fullname, email, company, message, privacy} = values
 
-    let {fullname, email} = values
+    let errors = {}
 
-    if (fullname.length < 2) {
-        alert("число символов не должно быть меньше 2")
-        return
+    if (fullname.length < 2 || fullname.length > 30) {
+        errors.fullname = "Имя не должно быть короче 2 символов и длиннее 30 символов"
+
     }
 
     let mailPattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
     if (!mailPattern.test(email)) {
-        alert("Email введен некорректно")
+        errors.email = "Email введен некорректно"
+
     }
 
-    alert(`Congratulations! Name: ${fullname}, Email: ${email}`)
+    if (company.length < 2) {
+        errors.company = "Название компании не должно быть короче 2 символов"
+    }
+
+    if (message.length < 2 || message.length > 300) {
+        errors.message = "Текст сообщения не должен быть короче 2 символов и длиннее 300 символов"
+
+    }
+
+    if (!myForm.privacy.checked) {
+        errors.privacy = "Пожалуйста отметьте галочку"
+    }
+
+
+
+
+    for (let el of myForm.elements) {
+        let name = el.getAttribute('name')
+        if (name in errors) {
+            el.classList.add("error")
+        } else {
+            el.classList.remove("error")
+        }
+    }
+
+    if (Object.keys(errors).length > 0) {
+        alert('Поля заполнены неверно')
+    } else {
+        alert(`Congratulations! Name: ${fullname}, Email: ${email}`)
+    }
+
 })
 
